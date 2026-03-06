fcd03c76d32d1b57287b319ec4cf6840cd2cc14f
Jackson Beard <6334208+Starman10@users.noreply.github.com>
2026-03-05 22:18:02 -0800

    Update Impact.js - updated numbers

--- components/Impact/Impact.js
+++ components/Impact/Impact.js
@@ -47,7 +47,7 @@ export default function Impact(props) {
           <Grid container spacing={0} justifyContent="center" alignItems="center" className={classes.metricGrid}>
             <Grid item size={{xs: 6}} className={classes.metricGridItem}>
                 <Typography className={classes.metricValue}>
-                  35
+                  42
                 </Typography>
                 <Typography className={classes.metricLabel}>
                   Patients helped
@@ -55,7 +55,7 @@ export default function Impact(props) {
             </Grid>
             <Grid item size={{xs: 6}} className={classes.metricGridItem}>
               <Typography variant="h5" className={classes.metricValue}>
-                24,285 miles
+                29,140 miles
               </Typography>
               <Typography variant="h5" className={classes.metricLabel}>
                 Travel paid for
